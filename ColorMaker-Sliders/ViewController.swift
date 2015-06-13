c464b8eed4df9b09e4b9c2a5a7b6a1e55d1ca7ad
Alex Paul <axp_alpaul@yahoo.com>
2015-06-12 21:24:51 -0400

    Completed Implementation

--- ColorMaker-Sliders/ViewController.swift
+++ ColorMaker-Sliders/ViewController.swift
@@ -9,17 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    // IBOutlets
+    @IBOutlet weak var redColorSlider: UISlider!
+    @IBOutlet weak var greenColorSlider: UISlider!
+    @IBOutlet weak var blueColorSlider: UISlider!
+    @IBOutlet weak var colorView: UIView!
+    
+    // Changes the color of the view based on the values of the three (3) Sliders
+    @IBAction func colorSliderChanged(sender: UISlider) {
+        // Get the current values of ALL (3) Color Sliders
+        // Convert UISlider float value to UIColor CGFloat
+        let redSliderValue = CGFloat(self.redColorSlider.value)
+        let greenSliderValue = CGFloat(self.greenColorSlider.value)
+        let blueSliderValue = CGFloat(self.blueColorSlider.value)
+        
+        self.colorView.backgroundColor = UIColor(red: redSliderValue, green: greenSliderValue, blue: blueSliderValue, alpha: 1)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
